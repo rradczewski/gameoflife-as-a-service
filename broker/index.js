c540e4e4013c40ff10a071a408e7dba250b1cdd1
Raimo Radczewski <raimo.radczewski@vaamo.de>
2016-06-20 23:54:34 +0200

    Add cors

--- broker/index.js
+++ broker/index.js
@@ -1,22 +1,24 @@
 const express = require('express');
 const { json } = require('body-parser');
+const cors = require('cors');
 
 const boards = {};
 
 const app = express();
 app.set('port', (process.env.PORT || 5000));
 
+app.use(cors());
 
 app.get('/board/:id', (req, res) => {
   if(!boards[req.id]) {
-    res.send(404);
+    return res.sendStatus(404);
   }
   res.send(boards[req.id]);
 });
 
 app.post('/board/:id', json(), (req, res) => {
   boards[req.id] = req.body;
-  res.send(200);
+  res.sendStatus(200);
 });
 
 app.listen(app.get('port'));
