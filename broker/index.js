eed0143a0bf45020922193025164bbd5d29203de
Raimo Radczewski <raimo.radczewski@vaamo.de>
2016-06-21 01:02:37 +0200

    Allow larger bodies and fix param #shouldveusedTDD

--- broker/index.js
+++ broker/index.js
@@ -10,15 +10,19 @@ app.set('port', (process.env.PORT || 5000));
 app.use(cors());
 
 app.get('/board/:id', (req, res) => {
-  if(!boards[req.id]) {
+  if(!boards[req.params.id]) {
+    console.error('No such board', req.params.id);
     return res.sendStatus(404);
   }
-  res.send(boards[req.id]);
+  res.send(boards[req.params.id]);
 });
 
-app.post('/board/:id', json(), (req, res) => {
-  boards[req.id] = req.body;
+app.post('/board/:id', json({limit: '50mb'}), (req, res) => {
+  boards[req.params.id] = req.body;
+  console.log('New Board for', req.params.id);
   res.sendStatus(200);
 });
 
-app.listen(app.get('port'));
+app.listen(app.get('port'), () => {
+  console.log('Broker is running on Port', app.get('port'));
+});
